81f83ebd0e73d4b0063dc9d05b1453ff3ccd042f
Manoj <manojkumar.bweb@gmail.com>
2018-09-05 11:11:44 +0530

    Initial commit

---
+++ app/src/main/java/com/manoj/fund/Login_Signup.java
@@ -0,0 +1,13 @@
+package com.manoj.fund;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login_Signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login__signup);
+    }
+}
